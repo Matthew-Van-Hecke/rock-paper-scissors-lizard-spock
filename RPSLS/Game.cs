0921b582c95eac6b737d5b3206a6109c28b9a8b3
Matthew VH <vanheckematt@gmail.com>
2020-02-13 09:50:41 -0600

    Version to be submitted: Reordered where the methods are defined in the game class to match the order of when they are first run. Fixed round counter so that it resets to 1 when new game is started. Slight changes to look of printed instructions (each member of move hierarchy indented, and instead of saying that play is to x number of points, it now says that play is to best of y.

--- RPSLS/Game.cs
+++ RPSLS/Game.cs
@@ -26,6 +26,7 @@ namespace RPSLS
             int roundCounter = 1;
             do
             {
+                roundCounter = 1;
                 Console.Clear();
                 gameLog.Clear();
                 //Print game instructions
@@ -47,48 +48,32 @@ namespace RPSLS
             } while (PlayAgain());
             Console.WriteLine("Goodbye, and thanks for playing!");
         }
-        private int PlayRound(int counter)
+        private void PrintScreen(int counter)
         {
-            string resultOfBattle;
-            //Let user(s) choose which gesture they want to use, clearing console after player one has made choice.
-            playerOne.PickGesture();
-            Console.Clear();
-            PrintScreen(counter);
-            Console.WriteLine("\nThank you for making your choice, " + playerOne.name + ".");
-            playerTwo.PickGesture();
-            //Run method which compares gestures to determine winner and returns result as a string. Save result string to a variable.
-            resultOfBattle = Battle(playerOne.gesture, playerTwo.gesture);
-            //Print who just one the round.
-            LogRoundResult(resultOfBattle, counter);
-            counter++;
-            //Clear console, and reprint.
-            Console.Clear();
-            PrintScreen(counter);
-            return counter;
+            PrintInstructions();
+            foreach (string logItem in gameLog)
+            {
+                Console.WriteLine(logItem);
+            }
+            if (counter > 1)
+            {
+                Console.WriteLine($"\nCurrent scores - {playerOne.name}: {playerOne.score} ---- {playerTwo.name}: {playerTwo.score}");
+                Console.WriteLine("--------------------------------------------");
+            }
         }
         private void PrintInstructions()
         {
             Console.WriteLine("Welcome to Rock-Paper-Scissors-Lizard-Spock! \n");
             Console.WriteLine("The rules are simple. You and your opponent will each choose a play (Rock, Paper, Scissors, Lizard, or Spock), and the player with the winning play gains a point. \n");
-            Console.WriteLine("The hierarchy of plays is as follows: \n Rock crushes Scissors \n Scissors cuts Paper \n Paper covers Rock \n Rock crushes Lizard \n Lizard poisons Spock \n Spock smashes Scissors \n Scissors decapitates Lizard \n Lizard eats Paper \n Paper disproves Spock \n Spock vaporizes Rock \n");
-            Console.WriteLine("The first player to " + winningScore + " points wins the game. Good luck! \n");
+            Console.WriteLine("The hierarchy of plays is as follows: \n   - Rock crushes Scissors \n   - Scissors cuts Paper \n   - Paper covers Rock \n   - Rock crushes Lizard \n   - Lizard poisons Spock \n   - Spock smashes Scissors \n   - Scissors decapitates Lizard \n   - Lizard eats Paper \n   - Paper disproves Spock \n   - Spock vaporizes Rock \n");
+            Console.WriteLine("Play continues until best of " + (winningScore * 2 - 1) + " is reached. Good luck! \n");
         }
-        private string Battle(Gesture playerOneGesture, Gesture playerTwoGesture)
+        private void PressKeyToContinue()
         {
-            //Look at gestures to see if either contains the other as one of its loser string variables.
-            //If one contains the name of the other, it wins. Increment that players score.
-            if (playerTwoGesture.beats.Contains(playerOneGesture.name))
-            {
-                playerTwo.score += 1;
-                return playerTwo.name;
-            }
-            else if (playerOneGesture.beats.Contains(playerTwoGesture.name))
-            {
-                playerOne.score += 1;
-                return playerOne.name;
-            }
-            //If both players picked the same object, they tie and nothing happens.
-            return "tie";
+            Console.WriteLine("------------------------------------------");
+            Console.Write("Press any key to continue: ");
+            Console.ReadKey();
+            Console.WriteLine("\n------------------------------------------");
         }
         private void ChoosePlayMode()
         {
@@ -105,51 +90,6 @@ namespace RPSLS
                 Console.WriteLine();
                 validSelection = ProcessKeyInputFromChoosePlayMode(keyUserInput);
             } while (!validSelection);
-        }
-        private void PressKeyToContinue()
-        {
-            Console.WriteLine("------------------------------------------");
-            Console.Write("Press any key to continue: ");
-            Console.ReadKey();
-            Console.WriteLine("\n------------------------------------------");
-        }
-        private void PrintScreen(int counter)
-        {
-            PrintInstructions();
-            foreach (string logItem in gameLog)
-            {
-                Console.WriteLine(logItem);
-            }
-            if (counter>1)
-            {
-                Console.WriteLine($"\nCurrent scores - {playerOne.name}: {playerOne.score} ---- {playerTwo.name}: {playerTwo.score}");
-                Console.WriteLine("--------------------------------------------");
-            }
-        }
-        private void PrintVictoryMessage()
-        {
-            if (playerOne.score >= winningScore)
-            {
-                Console.WriteLine("\n" + playerOne.name + " wins the game with a score of: " + playerOne.score);
-            }
-            else
-            {
-                Console.WriteLine("\n" + playerTwo.name + " wins the game with a score of: " + playerTwo.score);
-            }
-        }
-        private void LogRoundResult(string resultOfBattle, int counter)
-        {
-            string roundResult;
-            if (resultOfBattle == "tie")
-            {
-                roundResult = "\nRound was a tie";
-            }
-            else
-            {
-                roundResult = "\n" + resultOfBattle + " wins the round";
-            }
-            roundResult = "\nRound " + counter + ":\n" + playerOne.name + " picked " + playerOne.gesture.name + ".\n" + playerTwo.name + " picked " + playerTwo.gesture.name + "." + roundResult;
-            gameLog.Add(roundResult);
         }
         private bool ProcessKeyInputFromChoosePlayMode(ConsoleKeyInfo userInput)
         {
@@ -169,6 +109,67 @@ namespace RPSLS
                     return false;
             }
         }
+        private int PlayRound(int counter)
+        {
+            string resultOfBattle;
+            //Let user(s) choose which gesture they want to use, clearing console after player one has made choice.
+            playerOne.PickGesture();
+            Console.Clear();
+            PrintScreen(counter);
+            Console.WriteLine("\nThank you for making your choice, " + playerOne.name + ".");
+            playerTwo.PickGesture();
+            //Run method which compares gestures to determine winner and returns result as a string. Save result string to a variable.
+            resultOfBattle = Battle(playerOne.gesture, playerTwo.gesture);
+            //Print who just one the round.
+            LogRoundResult(resultOfBattle, counter);
+            counter++;
+            //Clear console, and reprint.
+            Console.Clear();
+            PrintScreen(counter);
+            return counter;
+        }
+        private string Battle(Gesture playerOneGesture, Gesture playerTwoGesture)
+        {
+            //Look at gestures to see if either contains the other as one of its loser string variables.
+            //If one contains the name of the other, it wins. Increment that players score.
+            if (playerTwoGesture.beats.Contains(playerOneGesture.name))
+            {
+                playerTwo.score += 1;
+                return playerTwo.name;
+            }
+            else if (playerOneGesture.beats.Contains(playerTwoGesture.name))
+            {
+                playerOne.score += 1;
+                return playerOne.name;
+            }
+            //If both players picked the same object, they tie and nothing happens.
+            return "tie";
+        }
+        private void LogRoundResult(string resultOfBattle, int counter)
+        {
+            string roundResult;
+            if (resultOfBattle == "tie")
+            {
+                roundResult = "\nRound was a tie";
+            }
+            else
+            {
+                roundResult = "\n" + resultOfBattle + " wins the round";
+            }
+            roundResult = "\nRound " + counter + ":\n" + playerOne.name + " picked " + playerOne.gesture.name + ".\n" + playerTwo.name + " picked " + playerTwo.gesture.name + "." + roundResult;
+            gameLog.Add(roundResult);
+        }
+        private void PrintVictoryMessage()
+        {
+            if (playerOne.score >= winningScore)
+            {
+                Console.WriteLine("\n" + playerOne.name + " wins the game with a score of: " + playerOne.score);
+            }
+            else
+            {
+                Console.WriteLine("\n" + playerTwo.name + " wins the game with a score of: " + playerTwo.score);
+            }
+        }
         public bool PlayAgain()
         {
             do
@@ -184,7 +185,7 @@ namespace RPSLS
                     case ("yeah"):
                         return true;
                     case (""):
-                        Console.WriteLine("Field cannot be left blank. Please type some response (\"y\", \"yes\", \"yep\", \"sure\", \"ya\", and \"yeah\" will all start a new game. Any other response will close the program.");
+                        Console.WriteLine("Field cannot be left blank. Please type some response (\"y\", \"yes\", \"yep\", \"sure\", \"ya\", and \"yeah\" will all start a new game. Any other response will close the program.\n");
                         break;
                     default:
                         return false;
